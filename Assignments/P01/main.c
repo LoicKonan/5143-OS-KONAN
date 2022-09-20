b43e46f31eb737c2bc7e07f97373da36d50b3b2c
Loic <konanloic10@gmail.com>
2022-09-20 17:17:05 -0500

    README.md

--- Assignments/P01/main.c
+++ Assignments/P01/main.c
@@ -3,25 +3,27 @@
  * @author:         Loic Konan
  * @brief:          This is the main file for the project.
  * @date:           2022-09-15
- *
+ * 
  * @Description:    This is the main file for the project.
- *                  C program which will run in a UNIX multiprocessor process.
-                    The program will read in two Numbers: A and B from the user
+ *                  C program which will run in a UNIX multiprocessor process. 
+                    The program will read in two Numbers: A and B from the user 
                     and compute 3 different results: A + B, A - B and A * B.
                     The program will Forks 2 other processes in order to do all 3 operations simultaneously.
-                    The program display the results, The program should print the results in the following format:
+                    The program will display the results.
+                    The program should print the results in the following format:
                     A + B = Addition
                     A - B = Subtraction
                     A * B = Multiplication
-                    Check the sequence of the three operations in your program
+                    Check the sequence of the three operations in your program 
                     and the order of appearance of the results in the screen.
                     Check the results with the sequential version of the program.
 
-* @version:        1.0
 
-* @bug:            Only the multiplication is working.
-
+* @Comments:        It seems like there is no order on how the results are printed on the screen, 
+                    sometimes it shows addition process id first, sometimes it is the multiplication 
+                    id first or the result of the multiplication first. it just not consistent.
  */
+
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -31,15 +33,14 @@
 
 int main()
 {
+    // Declare variables
     int A;
     int B;
     int Addition;
     int Subtraction;
     int Multiplication;
     int status;
-
-    pid_t my_pid;
-    my_pid = fork();
+    pid_t pid;
 
     // This read in A from the user
     printf("Enter A: ");
@@ -49,15 +50,64 @@ int main()
     printf("Enter B: ");
     scanf("%d", &B);
 
+    // This forks the first child
+    pid = fork();
 
-    if (my_pid < 0)
+    // This checks if the fork failed
+    if (pid < 0)
     {
-        printf("The fork didn't work! Terminate\n");
+        // This prints an error message
+        printf("Fork failed!\n");
+        exit(1);
+    }
+
+    // This checks if this is the first child
+    if (pid == 0)
+    {
+        // This computes the addition
+        Addition = A + B;
+
+        
+        // This prints the addition process id 
+        printf("Addition process id: %d \n", getpid());
+
+        // This prints the addition
+        printf(" Addition: A + B = %d \r \n", Addition);
+
+        // This exits the first child
         exit(0);
     }
 
-    // Parent block
-    if (my_pid != 0)
+    // This forks the second child
+    pid = fork();
+
+    // This checks if the fork failed
+    if (pid < 0)
+    {
+        // This prints an error message
+        printf("Fork failed!\n");
+        exit(1);
+    }
+
+    // This checks if this is the second child
+    if (pid == 0)
+    {
+        // This computes the subtraction
+        Subtraction = A - B;
+
+        
+        // This prints the subtraction process id
+        printf("Subtraction process id: %d \n", getpid());
+
+        // This prints the subtraction
+        printf(" Subtraction: A - B = %d \r \n", Subtraction);
+
+        // This exits the second child
+        exit(0);
+    }
+
+    // Parent process waits for the children to finish
+   else
     {
         // This computes the multiplication
         Multiplication = A * B;
@@ -66,7 +116,7 @@ int main()
         printf("Multiplication process id: %d \n", getpid());
 
         // This prints the multiplication
-        printf("Multiplication: A * B = %d \r \n", Multiplication);
+        printf(" Multiplication: A * B = %d \r \n", Multiplication);
 
         // This waits for the first child to finish
         waitpid(-1, &status, 0);
@@ -76,68 +126,6 @@ int main()
     }
 
 
-    // This forks the first child
-    my_pid = fork();
-
-    // This checks if the fork failed
-    if (my_pid < 0)
-    {
-        // This prints an error message
-        printf("Fork failed!\n");
-        exit(1);
-    }
-
-    // This is the first child block
-    if (my_pid == 0)
-    {
-        // This computes the addition
-        Addition = A + B;
-
-        // This prints the addition process id
-        printf("Addition process id: %d \n", getpid());
-
-        // This prints the addition
-        printf("Addition: A + B = %d \r \n", Addition);
-
-        // This waits for the children to finish
-        wait(&status);
-
-        // This prints the first child process id
-        printf("First child process id: %d \n", getpid());
-        
-        exit(0);
-    }
-
-    // This forks the second child
-    my_pid = fork();
-
-    // This checks if the fork failed
-    if (my_pid < 0)
-    {
-        // This prints an error message
-        printf("Fork failed!\n");
-        exit(1);
-    }
-
-    // This is the second child block
-    else if (my_pid == 0)
-    {
-        // This computes the subtraction
-        Subtraction = A - B;
-
-        // This prints the subtraction process id
-        printf("Subtraction process id: %d \n", getpid());
-
-        // This prints the subtraction
-        printf("Subtraction: A - B = %d \r \n", Subtraction);
-
-        // This prints the second child process id
-        printf("Second child process id: %d \n", getpid());
-        
-        exit(0);
-    }
-
+    // This exits the parent
     exit(0);
-
-    return 0;
 }
