eb4488f7a9c3f6ae9b660f6e8634f2df6f50120e
Loic <konanloic10@gmail.com>
2022-10-12 01:45:51 -0500

    README.md

--- Assignments/P02/main.cpp
+++ Assignments/P02/main.cpp
@@ -1,146 +1,49 @@
+// Your assignment consists of designing and implementing a program that will analyze the use of a paged
+// memory system with 32 Kbytes of physical memory. Your program will read the file and identify the number of page faults and the simulated overhead time as specified below.
+// The size of a page will be selected during the execution so the user can test two different page sizes. The
+// data file has 1 million addresses. Your program will accept several records of input data, according to the
+// following format:
+// Code Address (in hexadecimal)
+// Where code is identified by a single digit with the following meaning:
+// 0 - address for data read
+// 1 - address for data write
+// 2 - address for instruction fetch
+// An example of the data file contents could be
+// 2 415130
+// 0 1010acac
+// 2 415134
+
+// During the execution of this file, pages will be stored in the physical memory and later replaced following
+// a First In First Out algorithm. Pages accessed should be marked as referenced and those with a writing code
+// should be marked as modified. During page replacement, the overhead time will be increased by 100 cycles
+// due to the disk load operation and if the page has been modified another 500 cycles should be added to the
+// overhead to account for the writing back in disk. The experiment should be repeated with a Least Recently
+// Used algorithm (hint: a linked list or a queue may help you to track the LRU information)
+// Report your times for page size 4096 and 2048, running under FIFO and LRU page replacements.
+// You must turn in the source code of your program and a short report. This report must contain the
+// comparison of the two possible line sizes.
+
+
 #include <iostream>
-#include <math.h>
-#include <string.h>
 #include <fstream>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-struct processor
+
+struct page
 {
-    int frame_numbers;
+    int page_number;
     int valid_bit;
-    int refbits;
-    int modbits;
+    int ref_bit;
+    int mod_bit;
+    int time_stamp;
 };
-
-#define BLOCK_SIZE 4096
-#define Physical_memory 65536
-
-void swap_positions(struct processor *dup, int i);
 
 int main()
 {
-    ifstream infile;
-    infile.open("spice.txt");
-
-    int num;
-    string val;
-    char address[8] = {};
-    int address_val;
-    int code;
-
-    int offset_length = log2(BLOCK_SIZE);
-    int frames = Physical_memory / BLOCK_SIZE;
-    int pages_length = 20;
-    int page_val = 0;
-    int i = 0;
-    int available = 0;
-    int a = 0;
-    int faults = 0;
-
-    processor *algo = new processor[16];
-    for (i; i < frames; i += 1)
-    {
-        algo[i].frame_numbers = -1;
-        algo[i].valid_bit = 0;
-        algo[i].refbits = 0;
-        algo[i].modbits = 0;
-    }
-    while (infile >> num >> val)
-    {
-        code = num;
-        strcpy(address, val.c_str());
-        address_val = strtol(address, 0, 16);
-        page_val = address_val >> offset_length;
-
-        for (int i = 0; i < frames; i++)
-        {
-            if (algo[i].frame_numbers == page_val)
-            {
-                algo[i].refbits = 1;
-                algo[i].valid_bit = 1;
-                if (code == 1)
-                    algo[i].modbits = 1;
-                available = 1;
-            }
-        }
-        if (available == 0 && a < frames)
-        {
-
-            algo[a].frame_numbers = page_val;
-            algo[a].refbits = 1;
-            algo[a].valid_bit = 1;
-            if (code == 1)
-                algo[a].modbits == 1;
-            faults++;
-            a++;
-        }
-
-        else if (available == 0 && (a > frames - 1))
-        {
-
-            for (int k = 0; k < frames; k++)
-            {
-
-                if (algo[k].refbits == 1)
-                {
-                    algo[k].refbits = 0;
-                    swap_positions(algo, k);
-                    for (int z = 0; z < frames; z++)
-                    {
-                        algo[z].refbits = 0;
-                    }
-                }
-                if (algo[k].refbits == 0 && algo[k].modbits == 1)
-                {
-                    algo[k].modbits = 0;
-                    swap_positions(algo, k);
-                    for (int p = 0; p < frames; p++)
-                    {
-                        algo[p].refbits = 0;
-                    }
-                }
-                else if (algo[k].refbits == 0 && algo[k].modbits == 0)
-                {
-
-                    algo[k].frame_numbers = page_val;
-                    algo[k].refbits = 1;
-                    algo[k].valid_bit = 1;
-                    if (code == 1)
-                        algo[k].modbits = 1;
-                    swap_positions(algo, k);
-                    faults++;
-                    k = 100;
-                }
-            }
-        }
-        available = 0;
-    }
-    ofstream outfile;
-    outfile.open("output.txt");
-
-    outfile << "Number of page Faults: " << faults << endl;
-
-    outfile << "Frame Number \t"
-            << "Page Number \t"
-            << "Reference Bit\t  "
-            << "Modified Bit \t" << endl;
-    for (int i = 0; i < frames; i++)
-    {
-        outfile << i << " \t \t \t \t" << algo[i].frame_numbers << " \t \t \t \t " << algo[i].refbits << " \t \t \t \t" << algo[i].modbits << endl;
-    }
-    outfile.close();
-    infile.close();
-    return 0;
+    
 }
 
-void swap_positions(struct processor *dup, int i)
-{
-    processor a;
-    for (int l = i; l < 15; l++)
-    {
-        a = dup[l + 1];
-        dup[l + 1] = dup[l];
-        dup[l] = a;
-    }
-}
+
